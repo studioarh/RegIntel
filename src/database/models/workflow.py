e73bb2768594b578d0a144e8d4f695ed02bf0102
Abdul <309017486+studioarh@users.noreply.github.com>
2026-08-21 19:29:12 +0100

    feat: enhance WorkflowRun model with outcome and additional fields for persistence

--- src/database/models/workflow.py
+++ src/database/models/workflow.py
@@ -1,12 +1,24 @@
 from apps.db.session import Base
 import uuid
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import UUID, JSONB
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import ForeignKey, Enum
+from sqlalchemy import (
+    ForeignKey, 
+    Enum,
+    Text
+    )
 import enum
+import json
 
 class WorkflowType(str, enum.Enum):
     REG_DOC_ANALYSIS = "regulatory_document_analysis"
+
+
+class WorkflowOutcome(str, enum.Enum):
+    APPROVED = "approved"
+    REVIEW_REQUIRED = "review_required"
+    FAILED = "failed"
+    CRASHED = "crashed"
 
 
 
@@ -19,18 +31,59 @@ class WorkflowRun(Base):
         default=uuid.uuid4
     )
 
-    document_id: Mapped[uuid.UUID] = mapped_column(
+    document_id: Mapped[UUID] = mapped_column(
         ForeignKey("documents.id"),
         nullable=False
     )
 
     trace_id: Mapped[uuid.UUID] = mapped_column(
             UUID(as_uuid=True),
+            nullable=False,
             default=uuid.uuid4
-        )
+    )
 
     workflow_type: Mapped[WorkflowType] = mapped_column(
-        Enum(WorkflowType, name=""),
+        Enum(WorkflowType, name="workflow_type"),
         nullable=False,
         default=WorkflowType.REG_DOC_ANALYSIS
     )
+
+    retrieval_config_version: Mapped[str] = mapped_column(
+        Text,
+        nullable=False
+    )
+
+    prompt_version: Mapped[str] = mapped_column(
+        Text,
+        nullable=False
+    )
+
+    state_json: Mapped[json] = mapped_column(
+        JSONB,
+        nullable=False
+    )
+
+    output_json: Mapped[json] = mapped_column(
+        JSONB,
+        nullable=False
+    )
+
+    outcome: Mapped[enum.Enum] = mapped_column(
+        Enum(WorkflowOutcome, name="workflow_outcome"),
+        nullable=False
+    )
+
+
+
+class ReviewTasks(Base):
+    workflow_run_id: Mapped[UUID] = mapped_column(
+        ForeignKey("workflow_runs.id"),
+        nullable=False
+    )
+
+    document_id: Mapped[UUID] = mapped_column(
+            ForeignKey("documents.id"),
+            nullable=False
+        )
+
+
